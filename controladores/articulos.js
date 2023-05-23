91c9028ee294b2407a6dd3c1b3175db9e8f75963
Walter Lauxmann <walterlauxmann@institutocedec.com>
2023-05-23 20:25:07 -0300

    version con objetos - clase 23-05

--- controladores/articulos.js
+++ controladores/articulos.js
@@ -1,55 +1,107 @@
+const articulos= [
+  {
+  codigo: 101,
+  nombre: "iPhone SE",
+  descripcion: {
+    procesador: "A13 Bionic",
+    almacenamiento: "128 GB",
+    camaras: "7 Mpx / 12 Mpx",
+    pantalla: "4,7 Retina"
+  },
+  precio: 74999,
+  imagen: "iPhone-SE-Black.jpg"
+},
+{
+  codigo: 102,
+  nombre: "Motorola G100",
+  descripcion: {
+    procesador: "Octa-Core (3,2 GHz)",
+    almacenamiento: "128 GB",
+    camaras: "13 Mpx / 64 Mpx",
+    pantalla: "6,7 FHD+"
+  },
+  precio: 50000,
+  imagen: "Motorola-Moto-G100.jpg"
+},
+{
+  codigo: 103,
+  nombre: "iPhone 11 Pro",
+  descripcion: {
+    procesador: "A13 Bionic",
+    almacenamiento: "256 GB",
+    camaras: "12 Mpx / 12 Mpx",
+    pantalla: "5,8 Super Retina XDR"
+  },
+  precio: 232901,
+  imagen: "Iphone-11-Pro.jpg"
+},
+{
+  codigo: 201,
+  nombre: "Samsung Galaxy A72",
+  descripcion: {
+    procesador: "Octa-Core (2,3 GHz)",
+    almacenamiento: "128 GB",
+    camaras: "32 Mpx / 64 Mpx",
+    pantalla: "6,7 FHD+"
+  },
+  precio: 66999,
+  imagen: "Samsung-Galaxy-A72.jpg"
+}
+]
+
+const listado = document.querySelector("#listado"); // getElementById("listado")
+for(let articulo of articulos) {
+  listado.innerHTML += `
+              <div class="col">
+                <div class="card" style="width:18rem;">
+                    <img src="imagenes/productos/${articulo.imagen}" alt="${articulo.nombre}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <span name="spancodigo">${articulo.codigo}</span> - <span name="spannombre">${articulo.nombre}</span>
+                        </h5>
+                        <p class="card-text">
+                            Procesador: ${articulo.descripcion.procesador} <br>
+                            Almacenamiento: ${articulo.descripcion.almacenamiento} <br>
+                            Cámaras: ${articulo.descripcion.camaras} <br>
+                            Pantalla: ${articulo.descripcion.pantalla}.
+                        </p>
+                        <h5>$ <span name="spanprecio">${articulo.precio}</span></h5>
+                        <input type="number" name="inputcantidad" class="form-control" value="0" min="0" max="30" onchange="calcularPedido()">
+                    </div>
+                </div>
+            </div>
+`}
+
 function calcularPedido() {
-  const codigo1 = document.getElementById("spancodigo1").innerHTML;
-  const nombre1 = document.getElementById("spannombre1").innerHTML;
-  const precio1 = document.getElementById("spanprecio1").innerHTML;
-  const cantidad1 = document.getElementById("inputcantidad1").value;
+  const codigos = document.getElementsByName("spancodigo");
+  const nombres = document.getElementsByName("spannombre");
+  const precios = document.getElementsByName("spanprecio");
+  const cantidades = document.getElementsByName("inputcantidad");
+  const detalle = document.getElementById("detalle");
 
-  const codigo2 = document.getElementById("spancodigo2").innerHTML;
-  const nombre2 = document.getElementById("spannombre2").innerHTML;
-  const precio2 = document.getElementById("spanprecio2").innerHTML;
-  const cantidad2 = document.getElementById("inputcantidad2").value;
+  let cantidad;
+  let precio;
+  let totales = [];
+  let totalPedido = 0;
 
-  const codigo3 = document.getElementById("spancodigo3").innerHTML;
-  const nombre3 = document.getElementById("spannombre3").innerHTML;
-  const precio3 = document.getElementById("spanprecio3").innerHTML;
-  const cantidad3 = document.getElementById("inputcantidad3").value;
+  detalle.innerHTML = "";
 
-  const codigo4 = document.getElementById("spancodigo4").innerHTML;
-  const nombre4 = document.getElementById("spannombre4").innerHTML;
-  const precio4 = document.getElementById("spanprecio4").innerHTML;
-  const cantidad4 = document.getElementById("inputcantidad4").value;
-
-  let total1 = cantidad1 * precio1;
-  let total2 = cantidad2 * precio2;
-  let total3 = cantidad3 * precio3;
-  let total4 = cantidad4 * precio4;
-
-  let totalPedido = total1 + total2 + total3 + total4;
-
-  // Mostrar resultados
-  document.getElementById("codigo1").innerHTML = codigo1;
-  document.getElementById("nombre1").innerHTML = nombre1;
-  document.getElementById("cantidad1").innerHTML = cantidad1;
-  document.getElementById("precio1").innerHTML = "$ " + precio1 + ".-";
-  document.getElementById("total1").innerHTML = "$ " + total1 + ".-";
-
-  document.getElementById("codigo2").innerHTML = codigo2;
-  document.getElementById("nombre2").innerHTML = nombre2;
-  document.getElementById("cantidad2").innerHTML = cantidad2;
-  document.getElementById("precio2").innerHTML = "$ " + precio2 + ".-";
-  document.getElementById("total2").innerHTML = "$ " + total2 + ".-";
-
-  document.getElementById("codigo3").innerHTML = codigo3;
-  document.getElementById("nombre3").innerHTML = nombre3;
-  document.getElementById("cantidad3").innerHTML = cantidad3;
-  document.getElementById("precio3").innerHTML = "$ " + precio3 + ".-";
-  document.getElementById("total3").innerHTML = "$ " + total3 + ".-";
-
-  document.getElementById("codigo4").innerHTML = codigo4;
-  document.getElementById("nombre4").innerHTML = nombre4;
-  document.getElementById("cantidad4").innerHTML = cantidad4;
-  document.getElementById("precio4").innerHTML = "$ " + precio4 + ".-";
-  document.getElementById("total4").innerHTML = "$ " + total4 + ".-";
-
-  document.getElementById("totalpedido").innerHTML = "$ " + totalPedido + ".-";
+  for(let i=0; i < codigos.length; i++) { // para (desde i = 0 hasta que i sea menor a la longitud de codigos incrementando i de 1 en 1)
+    cantidad = cantidades[i].value;
+    precio = parseFloat(precios[i].innerHTML);
+    if(cantidad > 0) { // Si (cantidad es mayor que cero)
+      totales[i] = cantidad * precio;
+      totalPedido += totales[i]; // totalPedido = totalPedido + totales[i]
+      detalle.innerHTML += `
+        <tr>
+          <td>${codigos[i].innerHTML}</td>
+          <td>${nombres[i].innerHTML}</td>
+          <td>${cantidades[i].value}</td>
+          <td>${precios[i].innerHTML}</td>
+          <td>${totales[i]}</td>
+        </tr>
+      `;
+      document.getElementById("totalpedido").innerHTML= totalPedido;
+    }
+  }
 }
